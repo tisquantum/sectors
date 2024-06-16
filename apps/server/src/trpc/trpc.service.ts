27e8c5b5bc335bdd85afcbd9a3cbfc440e5d606f
tisquantum <whatyoudontunderstand@proton.me>
2024-06-16 11:30:40 -0700

    yes

--- apps/server/src/trpc/trpc.service.ts
+++ apps/server/src/trpc/trpc.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { initTRPC } from '@trpc/server';
-import SuperJSON from 'superjson';
-
-export const transformer = {
-  input: {
-    serialize: (object: unknown) => SuperJSON.stringify(object),
-    deserialize: (object: string) => SuperJSON.parse(object),
-  },
-  output: {
-    serialize: (object: unknown) => SuperJSON.stringify(object),
-    deserialize: (object: string) => SuperJSON.parse(object),
-  },
-};
+// HACK: The `superjson` library is ESM-only (does not support CJS), while our codebase is CJS.
+// This is a workaround to still get to use the latest version of the library from our codebase.
+// https://github.com/blitz-js/superjson/issues/268
+// https://www.npmjs.com/package/fix-esm
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
+const fixESM = require("fix-esm");
+// @ts-expect-error This is a type-only import, so won't get transformed to `require()`.
+import type SuperJSON from "superjson";
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
+const SuperJSON: SuperJSON = fixESM.require("superjson");
 
 @Injectable()
 export class TrpcService {
   trpc = initTRPC.create({
-    transformer: transformer,
+    transformer: SuperJSON,
   });
   procedure = this.trpc.procedure;
   router = this.trpc.router;
