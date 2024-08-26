9640554b43a2435084e866ed1c2b1d46b4898683
tisquantum <whatyoudontunderstand@proton.me>
2024-08-26 08:49:42 -0700

    sure

--- apps/sectors/app/login/actions.ts
+++ apps/sectors/app/login/actions.ts
@@ -21,8 +21,8 @@ export async function login(formData: FormData) {
     redirect('/error')
   }
 
-  revalidatePath('/', 'layout')
-  redirect('/')
+  revalidatePath('/rooms', 'layout')
+  redirect('/rooms')
 }
 
 export async function signup(formData: FormData) {
