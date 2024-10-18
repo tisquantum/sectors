9cd16405cfdae3a377138e84e8700143ca2a1981
tisquantum <whatyoudontunderstand@proton.me>
2024-10-17 21:28:27 -0700

    sure

--- apps/sectors/app/components/TopBar.tsx
+++ apps/sectors/app/components/TopBar.tsx
@@ -40,7 +40,7 @@ const CommonNavLinks = memo(() => (
       </Link>
     </NavbarItem>
     <NavbarItem className="flex items-center">
-      <Link href="https://discord.gg/Tdghbrx9" target="_blank">
+      <Link href={process.env.NEXT_PUBLIC_DISCORD_LINK} target="_blank">
         <RiDiscordFill />
       </Link>
     </NavbarItem>
